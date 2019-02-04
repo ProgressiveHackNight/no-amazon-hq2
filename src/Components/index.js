ba13c85f37ecd199b984a2fe36fb1956e73bec92
Abigail Demsas <ademsas@etsy.com>
2019-02-04 12:40:08 -0500

    moved react components into central export file

---
+++ src/Components/index.js
@@ -0,0 +1,9 @@
+export { default as ContactYourRep } from './ContactYourRep';
+export { default as DropDown } from './DropDown';
+export { default as EmailSignup } from './EmailSignup';
+export { default as Footer } from './Footer';
+export { default as Header } from './Header';
+export { default as LearnMore } from './LearnMore';
+export { default as Nav } from './Nav';
+export { default as RepCard } from './RepCard';
+export { default as TakeAction } from './TakeAction';
