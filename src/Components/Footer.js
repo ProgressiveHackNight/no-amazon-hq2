3b127224fcfe90134516d20b2cd6614401d9411d
M. Hilton <meredithjunior@gmail.com>
2019-02-10 11:10:41 -0500

    adding footer links, styling

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,13 +1,45 @@
 import React, { Component } from 'react';
+import twitter from '../utils/twitter.png';
+import facebook from '../utils/facebook.png';
+
 import '../css/Footer.css';
 
 class Footer extends Component {
+  constructor() {
+  	super();
+  	this.state ={
+  		links: [
+  			{
+  				link:"#",
+  				image:twitter,
+  				alt:"twitter"
+  			}, {
+  				link:"#",
+  				image:facebook,
+  				alt:"facebook"
+  			}, {
+  				link:"#",
+  				alt:"email",
+  				text:"Contact Us"
+  			}
+  		]
+  	}
+  }
+
   render() {
     return (
       <div className="Footer">
-          <h1>
-            Footer
-          </h1>
+      	<div className="center-container">
+      	{this.state.links.map((link) => {
+              return (
+              	<a href={link.link} alt={link.alt}>
+              		{link.image && <img className="social-logo" src={link.image}/>}
+              		{link.text && link.text}
+              	</a>
+              )
+            })
+          }
+         </div>
       </div>
     );
   }
