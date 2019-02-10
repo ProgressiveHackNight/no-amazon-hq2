07b21dc7a8be5fb6180eb3a451de24aaca706d44
Lindsay Sack <lsack@etsy.com>
2019-02-09 20:37:21 -0500

    updated header image and text alignment

--- src/Components/EmailSignup.js
+++ src/Components/EmailSignup.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import '../css/EmailSignup.css';
+import '../css/skeleton.css';
 
 class EmailSignup extends Component {
   render() {
@@ -8,6 +9,30 @@ class EmailSignup extends Component {
           <h1>
             Sign up for the mailing list
           </h1>
+          <form>
+              <div className="row">
+                  <div className="six columns">
+                      <label htmlFor="exampleEmailInput">Your email</label>
+                      <input className="u-full-width" type="email" placeholder="test@mailbox.com"
+                          id="exampleEmailInput"/>
+                  </div>
+                  <div className="six columns">
+                      <label htmlFor="exampleRecipientInput">Reason for contacting</label>
+                      <select className="u-full-width" id="exampleRecipientInput">
+                          <option value="Option 1">Questions</option>
+                          <option value="Option 2">Admiration</option>
+                          <option value="Option 3">Can I get your number?</option>
+                      </select>
+                  </div>
+              </div>
+              <label htmlFor="exampleMessage">Message</label>
+              <textarea className="u-full-width" placeholder="Hi Dave …" id="exampleMessage"></textarea>
+              <label className="example-send-yourself-copy">
+                  <input type="checkbox"/>
+                      <span className="label-body">Send a copy to yourself</span>
+              </label>
+              <input className="button-primary" type="submit" value="Submit"/>
+          </form>
       </div>
     );
   }
--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,15 +1,24 @@
 import React, { Component } from 'react';
 import logo from '../utils/hq2-frowny-face-01.png';
 import '../css/Header.css';
+import '../css/skeleton.css';
 
 class Header extends Component {
   render() {
     return (
       <div className="Header">
-          <img id="frowny-face" src={logo} alt="Amazon Frowny Face"/>
-          <h1 id="header-text">
-            No Amazon HQ2 in NYC!
-          </h1>
+          <div className="row">
+              <div className="one column">
+                  <h1 id="header-text">
+                      No Amazon HQ2 in NYC!
+                  </h1>
+              </div>
+              <div className="eleven columns">
+                  <img id="frowny-face" src={logo} alt="Amazon Frowny Face"/>
+              </div>
+          </div>
+
+
       </div>
     );
   }
