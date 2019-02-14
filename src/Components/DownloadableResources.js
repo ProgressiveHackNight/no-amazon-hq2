7f8957e2e92e39e19fd4ddf3940de0681bdda5eb
brad <ballen@uncommongoods.com>
2019-02-13 21:29:17 -0500

    render downloadable links from lists of ids and names, not just static html

--- src/Components/DownloadableResources.js
+++ src/Components/DownloadableResources.js
@@ -5,6 +5,52 @@ class DownloadableResources extends Component {
     constructor() {
         super()
         this.state = {
+            posters: [
+                {
+                    gDriveId: '1MssoXNyDOn7V7I0OpYDC7GT2E1mXv6hR',
+                    name: 'Amazon & I.C.E'
+                },
+                {
+                    gDriveId: '130gioYsJC-RztUcq31OrIRZ4um1ZlgLq',
+                    name: 'Amazon Delivers Lies'
+                },
+                {
+                    gDriveId: '19NTSTodUcCVbS7m1aVHJbY_pAkjl9i9F',
+                    name: 'Amazon Lies'
+                }
+            ],
+            stickers: [
+                {
+                    gDriveId: '1Y5sdk9AoDntgU2GG8yfD-Pyzb-zGqkSj',
+                    name: 'Worker Abuse'
+                },
+                {
+                    gDriveId: '1w9ifmzO1aZ6d95q951sSFbDBU20dmCtz',
+                    name: 'Higher Rents'
+                },
+                {
+                    gDriveId: '18Cfhu6D4ohB5rxcHo-rqW_f5HVOm6PbY',
+                    name: 'Homelessness'
+                },
+                {
+                    gDriveId: '1-hZIiTV2aXjnBc8Y5ruDHDXpiqeysCXr',
+                    name: 'Corporate Handouts'
+                },
+                {
+                    gDriveId: '13qSGAdtVTy7opP_Yk4ZnHLemV2CIYve9',
+                    name: 'Deportations'
+                },
+                {
+                    gDriveId: '1zbRLw26le36K6vdBTj9VwcjRpPKWtv28',
+                    name: '#noamazonnyc'
+                }
+            ],
+            factSheets: [
+                {
+                    gDriveId: '0B2MrkrgsB-jNR1UyM2ktVUFza3pfNkY0Qm82dHNwNTEybkxJ',
+                    name: 'Brand Hijacking'
+                },
+            ],
             showPoster: false,
             showSticker: false,
             showFactSheet: false
@@ -29,6 +75,24 @@ class DownloadableResources extends Component {
     }
 
     render() {
+        const posters = this.state.posters.map(poster => {
+            const url = `https://drive.google.com/uc?export=download&id=${poster.gDriveId}`
+            return (
+                <div className="four columns">
+                    <a className="Downloadable" href={url}>{poster.name}</a>
+                </div>
+            )
+        })
+
+        const stickers = this.state.stickers.map((sticker, idx) => {
+            const url = `https://drive.google.com/uc?export=download&id=${sticker.gDriveId}`
+            return (
+                <div className={`${idx > 0 && idx%4===0?'offset-by-three ':''}three columns`}>
+                    <a className="Downloadable" href={url}>{sticker.name}</a>
+                </div>
+            )
+        })
+
         return (
             <div className="DownloadableResources">
                 <h2>
@@ -42,15 +106,7 @@ class DownloadableResources extends Component {
                     {
                         this.state.showPoster &&
                             <div className="row">
-                                <div className="four columns">
-                                    <a className="Downloadable" href="https://drive.google.com/uc?export=download&id=1MssoXNyDOn7V7I0OpYDC7GT2E1mXv6hR">Amazon & I.C.E</a>
-                                </div>
-                                <div className="four columns">
-                                    <a className="Downloadable" href="https://drive.google.com/uc?export=download&id=130gioYsJC-RztUcq31OrIRZ4um1ZlgLq">Amazon Delivers Lies</a>
-                                </div>
-                                <div className="four columns">
-                                    <a className="Downloadable" href="https://drive.google.com/uc?export=download&id=19NTSTodUcCVbS7m1aVHJbY_pAkjl9i9F">Amazon Lies</a>
-                                </div>
+                                { posters }
                             </div>
                     }
                 <div className="row">
@@ -62,26 +118,7 @@ class DownloadableResources extends Component {
                     this.state.showSticker &&
                     <div>
                         <div className="row">
-                            <div className="three columns">
-                                <a className="Downloadable" href="https://drive.google.com/uc?export=download&id=1Y5sdk9AoDntgU2GG8yfD-Pyzb-zGqkSj">Worker Abuse</a>
-                            </div>
-                            <div className="three columns">
-                                <a className="Downloadable" href="https://drive.google.com/uc?export=download&id=1w9ifmzO1aZ6d95q951sSFbDBU20dmCtz">Higher Rents</a>
-                            </div>
-                            <div className="three columns">
-                                <a className="Downloadable" href="https://drive.google.com/uc?export=download&id=18Cfhu6D4ohB5rxcHo-rqW_f5HVOm6PbY">Homelessness</a>
-                            </div>
-                            <div className="three columns">
-                                <a className="Downloadable" href="https://drive.google.com/uc?export=download&id=1-hZIiTV2aXjnBc8Y5ruDHDXpiqeysCXr">Corporate Handouts</a>
-                            </div>
-                        </div>
-                        <div className="row">
-                            <div className="offset-by-three three columns">
-                                <a className="Downloadable" href="https://drive.google.com/uc?export=download&id=13qSGAdtVTy7opP_Yk4ZnHLemV2CIYve9">Deportations</a>
-                            </div>
-                            <div className="three columns">
-                                <a className="Downloadable" href="https://drive.google.com/uc?export=download&id=1zbRLw26le36K6vdBTj9VwcjRpPKWtv28">#noamazonnyc</a>
-                            </div>
+                            { stickers }
                         </div>
                     </div>
 
@@ -95,7 +132,7 @@ class DownloadableResources extends Component {
                     this.state.showFactSheet &&
                     <div className="row">
                         <div className="offset-by-four four columns">
-                            <a className="Downloadable" href="https://drive.google.com/uc?export=download&id=0B2MrkrgsB-jNR1UyM2ktVUFza3pfNkY0Qm82dHNwNTEybkxJ">Brand Hijacking</a>
+                            <a className="Downloadable" href={`https://drive.google.com/uc?export=download&id=${this.state.factSheets[0].gDriveId}`}>{this.state.factSheets[0].name}</a>
                         </div>
                     </div>
                 }
