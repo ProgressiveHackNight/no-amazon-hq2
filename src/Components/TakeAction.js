60d0d40e0560e7ccf7e576358655a07b45beba10
Rapi Castillo <rapi.castillo@gmail.com>
2019-03-06 05:09:06 -0500

    Fix items

--- src/Components/TakeAction.js
+++ src/Components/TakeAction.js
@@ -3,7 +3,7 @@ import DropDown from './DropDown';
 import action from '../utils/take-action.jpg';
 import {takeActionContent} from '../utils/content';
 import '../css/TakeAction.css';
-
+import wide from '../utils/wide-img.png';
 class TakeAction extends Component {
   constructor(props){
     super(props);
@@ -14,7 +14,13 @@ class TakeAction extends Component {
   render() {
     return (
       <div className="TakeAction" id={this.props.id}>
-          <img src={action} alt="Woman holding sign Amazon Causes Gentrification"/>
+        <div className='TakeActionViewport'>
+          <h1>Open Letter to Amazon</h1>
+          <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?</p>
+          <a href='javascript: void(null)' className='btn'>Call to Action</a>
+        </div>
+        <img src={wide} />
+          {/*<img src={action} alt="Woman holding sign Amazon Causes Gentrification"/>
           <div id="action-text">
             <h1>
               Take Action
@@ -22,7 +28,7 @@ class TakeAction extends Component {
             {this.state.bullets.map((bullet)=>{
               return <DropDown key={bullet.headline} headline={bullet.headline} text={bullet.text}/> 
             })}
-          </div>
+          </div>*/}
       </div>
     );
   }
